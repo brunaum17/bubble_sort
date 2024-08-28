e7b9ede95a172ec8777d6addc7b3cf1f5342b979
Bruno Guimarães <bruno.noguimaraes@gmail.com>
2024-08-28 19:42:13 -0300

    update

--- funcao.py
+++ funcao.py
@@ -2,7 +2,7 @@ import time
 import random
 
 def gerar_lista():
-    return [random.randint(0, 10000) for _ in range(10000)]
+    return [random.randint(0, 1000) for _ in range(10000)]
 
 def bubble_sort(L):
 
@@ -13,12 +13,13 @@ def bubble_sort(L):
     n = len(L)
     for j in range(1, n):
         for i in range(0,n - 1):
+            troca_count += 1  # Incrementa o contador de trocas
             if L[i] > L[i + 1]:
                 # Troca os elementos de posição
                 t = L[i]
                 L[i] = L[i + 1]
                 L[i + 1] = t
-                troca_count += 1  # Incrementa o contador de trocas
+                
 
     end_time = time.time()  # Fim da medição do tempo
     tempo_execucao = end_time - start_time  # Tempo total de execução
